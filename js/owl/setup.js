9ee15c307299f2af57d2b5a974a6cdacd023a351
unknown <mauriciojuniorxd@gmail.com>
2022-01-12 18:51:01 -0300

    Primeiro commit

---
+++ js/owl/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({
+    loop: false,
+    margin: 8,
+    nav: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        400: {
+            items: 3
+        },
+        600: {
+            items: 5
+        }
+    }
+})
